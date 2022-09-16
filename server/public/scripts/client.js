0bccd1a43e6af09f05510c06fb7ba59d09349253
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-16 17:17:01 -0500

    working appendTasks in client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,42 @@
 $(onReady);
 
 function onReady() {
-    
+  clickHandler();
+  appendTasks();
 }
+
+function clickHandler() {
+  $("#addNoteBtn").on("click", addTask);
+}
+
+function checkError(error) {
+  console.log("error caught :>> ", error);
+}
+
+function appendTasks() {
+  $.ajax({
+    method: "GET",
+    url: "/tasks",
+  })
+    .then((results) => {
+      $("#noteList").empty();
+      console.log("results :>> ", results);
+
+      for (const task of results) {
+        $("#noteList").append(`
+            <tr data-is-complete="${task.is_complete}" data-id="${task.id}">
+                <td>${task.note}</td>
+                <td>${task.is_complete ? "Complete" : "Pending"}</td>
+                <td>
+                <button class="complete-button">Complete</button>
+                <button class="edit-button">Edit</button>
+                <button class="delete-button">Delete</button>
+                </td>
+            </tr>
+        `);
+      }
+    })
+    .catch(checkError);
+}
+
+function addTask() {}
