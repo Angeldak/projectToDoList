4fcaf0d85361ba56e9255450ec03f661d4cc74c8
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-26 22:23:41 -0500

    updated pool for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,24 @@
 const pg = require("pg");
+const url = require("url");
 
-const pool = new pg.Pool({
-  database: "weekend-to-do-app",
-  host: "localhost",
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000,
-});
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  config = {
+    host: "localhost",
+    port: 5432,
+    database: "weekend-to-do-app",
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+}
+
+const pool = new pg.Pool(config);
 
 pool.on("connect", () => {
   console.log("Connected to PostGres");
